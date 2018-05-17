ee5db75d0c22492b2085851619a5bd5b7007d46e
Jordan Gill <jordangill600@gmail.com>
2018-05-17 22:32:06 +0100

    Wizard-app

---
+++ apps/wizard_battle/program.py
@@ -0,0 +1,44 @@
+from actors import Wizard, Creature
+
+
+def main():
+    print_header()
+    game_loop()
+
+
+def print_header():
+    print('---------------------------')
+    print('      WIZARD GAME APP')
+    print('---------------------------')
+
+
+def game_loop():
+
+    creatures = [
+        Creature('Toad', 1),
+        Creature('Tiger', 12),
+        Creature('Bat', 3),
+        Creature('Dragon', 50),
+        Creature('Evil Wizard', 1000)
+    ]
+
+    print(creatures)
+    hero = Wizard('Gandolf', 75)
+
+
+    while True:
+
+        cmd = input('Do you want to [a]ttack, [r]unaway or [l]ook around? ')
+        if cmd == 'a':
+            print('attack')
+        elif cmd == 'r':
+            print('runaway')
+        elif cmd == 'l':
+            print('look around')
+        else:
+            print("OK, Exiting game... bye!")
+            break
+
+
+if __name__ == '__main__':
+    main()
