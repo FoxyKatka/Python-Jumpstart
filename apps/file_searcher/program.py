719510ced503f1b3a64cb17896c5f84454f333d3
Jordan Gill <jordangill600@gmail.com>
2018-05-20 18:10:52 +0100

    File-Search-App

--- apps/file_searcher/program.py
+++ apps/file_searcher/program.py
@@ -14,6 +14,10 @@ def main():
 
     search_folders(folder, text)
 
+    matches = search_folders(folder, text)
+    for m in matches:
+        print(m)
+
 
 def print_header():
     print('------------------------------')
@@ -34,11 +38,34 @@ def get_folder_from_user():
 
 def get_search_text_from_user():
     text = input('What are you searching for [single phrases only]? ')
-    return text
+    return text.lower()
 
 
 def search_folders(folder, text):
-    print("Would search {} for {}".format(folder, text))
+    # print("Would search {} for {}".format(folder, text))
+    all_matches = []
+    items = os.listdir(folder)
+
+    for item in items:
+        full_item = os.path.join(folder, item)
+        if os.path.isdir(item):
+            continue
+
+        matches = search_file(item, text)
+        all_matches.extend(matches)
+
+    return all_matches
+
+
+def search_file(filename, search_text):
+    matches = []
+    with open(filename, 'r', encoding='utf-8') as fin:
+
+        for line in fin:
+            if line.lower().find(search_text) >= 0:
+                matches.append(line)
+
+        return matches
 
 
 if __name__ == '__main__':
